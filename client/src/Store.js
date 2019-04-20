90f6c0b48f74a21fc9198af7ed198d85fb8a604f
zderick <centuryib100@gmail.com>
2019-04-20 11:08:12 -0700

    Update Store.js

--- client/src/Store.js
+++ client/src/Store.js
@@ -8,7 +8,9 @@ const middleware = [thunk];
 
 const store = createStore(rootReducer, initialState, compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.REDUX_DEVTOOLS_EXTENSION_COMPOSE ? window.REDUX_DEVTOOLS_EXTENSION_COMPOSE({
+    // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose
 ));
 
 export default store;
